e15d8abe5308ec56051509c2954be0f22ad42e8f
tuan anh <tuananh97aowvn@gmail.com>
2019-06-16 14:52:18 +0700

    fix UI

--- resources/views/components/slider.blade.php
+++ resources/views/components/slider.blade.php
@@ -2,8 +2,8 @@
     <div class="header-main">
         <div class="d-sm-flex justify-content-between">
             <a href=""><img src="{{ asset('web/images/logo.png') }}" style="height: 100px"></a>
-            <h4 style="color: white">Tri thức - Chắp cánh - Thành công</h4>
-            <div class="hearder-right-w3_pvt d-flex justify-content-sm-end align-items-center  mt-sm-0 mt-4">
+            <h4 class="sologan">Tri thức - Chắp cánh - Thành công</h4>
+            <div class="hearder-right-w3_pvt d-flex justify-content-sm-end align-items-center  mt-sm-0 mt-4 login-row">
                 <ul class="d-flex header-w3_pvt">
 
                     @if(empty(Auth::user()->id))
@@ -20,7 +20,7 @@
                     @else
                         <li>
                             <button type="button" class="btn w3ls-btn btn-2  d-block">
-                                <a href="{{ route('student.logout') }}"><span class="fa fa-sign-in"> Đăng xuất</span></a>
+                                <a href="{{ route('student.logout') }}"><span class="fa fa-sign-in"></span>Đăng xuất</a>
                             </button>
                         </li>
                     @endif
@@ -31,6 +31,6 @@
     </div>
 </div>
 <div class="callbacks_container">
-    <div style="background-color: #248ab2; padding: 80px"></div>
+    <div style="background-color: #248ab2; padding: 55px"></div>
 </div>
 <script type="text/javascript" src="{{ asset('js/login.js') }}"></script>
