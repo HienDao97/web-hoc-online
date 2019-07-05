83c32f9e30c205c28ae6df7ff5a05b2f18c13928
Nguyen Xuan Khoa <khoa1997hn@gmail.com>
2019-07-05 19:52:29 +0700

    them trang gioi thieu

--- resources/views/gioi-thieu/index.blade.php
+++ resources/views/gioi-thieu/index.blade.php
@@ -1,7 +1,32 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: paditech
- * Date: 6/19/19
- * Time: 4:54 PM
- */
+@extends('default')
+@section('title', 'Giới thiệu')
+@section('content')
+    <div class="portfolio-wthree py-lg-5" id="portfolio" style="background-color: #eee">
+        <section class="team_wthree py-4" id="team">
+            <div class="container py-lg-5">
+                <div class="sec-main">
+                    <span class="sub-line">Giới thiệu</span>
+                </div>
+                <div class="container-grid py-4 py-lg-5" style="font-size: 20px; background-color: white; padding: 20px">
+                	<p style="text-indent: 1.5em;">vutienthanh.com là website dạy toán cho học sinh tiểu học. Đây là website được xây dựng bởi ThS. Vũ Tiến Thành và những cộng sự tài năng, tâm huyết nhằm đem đến cho các em học sinh một địa chỉ học tập online thân thiện và hiệu quả cao.</p>
+                	<br>
+                	<p style="text-indent: 1.5em;">ThS. Vũ Tiến Thành tốt nghiệp cử nhân Khoa Giáo dục Tiểu học, Đại học Sư phạm Hà Nội – Trường Đại học số 1 Việt Nam về đào tạo Giáo viên tiểu học. Năm 2011, thầy hoàn thành luận văn Thạc sĩ Toán tiểu học với điểm xuất sắc. Thầy đã có 8 năm công tác và giảng dạy tại Khoa Tiểu học của trường ĐHSP Hà Nội. Trong thời gian đó, thầy đã giúp đỡ nhiều sinh viên, học sinh học tập đạt kết quả tốt. </p>
+                	<br>
+                	<p style="text-indent: 1.5em;">Hiện tại, thầy đang làm Trợ lí Hiệu trưởng – Tổ trưởng tổ Toán trường TH-THCS Pascal. Thầy đã giúp đỡ nhiều học sinh thi đỗ các kì thi quốc gia và quốc tế như : Toán IKMC, Toán Sasmo, Toán Titan,… </p>   
+                	<br>
+                	<p style="text-indent: 1.5em;">Thầy Vũ Tiến Thành là người sáng lập ra Bộ sơ đồ tư duy học Toán cho học sinh tiểu học. Nhờ bộ sơ đồ tư duy này, nhiều học sinh và giáo viên trong cả nước đã ứng dụng hiệu quả trong quá trình dạy và học. </p>
+                	<br>
+                	<p style="text-indent: 1.5em;">Thầy Vũ Tiến Thành là người đồng sáng lập kênh học toán dành cho học sinh tiểu học Simplemaths với gần 10 000 lượt Sub. Thầy đã chia sẻ kiến thức và kinh nghiệm dạy học cho cộng đồng giáo viên và học sinh trong cả nước.  </p>
+                	<br>
+                	<p style="text-indent: 1.5em;">Niềm đam mê của Thầy là dạy Toán cho học sinh tiểu học và tìm ra các phương pháp học tập mới, giúp cho các em học sinh học tập dễ dàng hơn. Giúp cho các em có niềm yêu thích toán học để trở thành những con người tài đức vẹn toàn, giúp ích cho quê hương và đất nước.</p>       
+                	<br>
+                	<p style="text-indent: 1.5em;">Website : Vutienthanh.com có nhiều khóa học. Chương trình được thiết kế theo chương trình của Bộ Giáo dục và Đào tạo. Với phương pháp giảng dạy dễ hiểu, video bài dạy thiết kế dưới dạng hoạt tạo ra sự hứng thú cho học sinh trong quá trình học tập.</p>      
+                	<br>
+                	<p style="text-indent: 1.5em;">Chúc các em học sinh luôn tìm được cho mình niềm đam mê học tập. Chúc các em có một kết quả học tập tốt nhất.</p>                   	            	                    	                	                  	             	
+                </div>
+            </div>
+        </section>
+    </div>
+
+@endsection
+
